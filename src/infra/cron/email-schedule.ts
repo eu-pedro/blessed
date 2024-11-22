eb194efafc70bb83e6c8185cc2d92e08bef32d6d
pedro henrique <pedro.henrike2812@gmail.com>
2024-11-22 10:59:43 -0300

    fix: alterando hora de rodar o job

--- src/infra/cron/email-schedule.ts
+++ src/infra/cron/email-schedule.ts
@@ -14,7 +14,7 @@ export async function scheduleDailyEmail() {
   const versesHistoryRepository = new PrismaVersesHistoryRepository(prismaClient)
   const sendDailyEmailUseCase = new SendDailyEmailUseCase(usersRepository, emailService, versesHistoryRepository)
 
-  cron.schedule("6 6 * * *", async () => {
+  cron.schedule("*/5 * * * *", async () => {
     if (isRunning) {
       console.log("Processo ainda em execução. Aguardando...");
       return;
